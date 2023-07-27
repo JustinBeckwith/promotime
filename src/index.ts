a5b66ae413fbcb2b7b04d2e5fea7fb43433ed155
Justin Beckwith <justin.beckwith@discordapp.com>
2023-07-27 15:38:32 -0700

    chore: move from gts to xo (#133)

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
+import fs from 'node:fs';
 import {BigQuery} from '@google-cloud/bigquery';
 import meow from 'meow';
-import updateNotifier from 'update-notifier';
-import fs from 'fs';
-import {URL} from 'url';
+import updateNotifier, {type Package} from 'update-notifier';
 
 const pkg = JSON.parse(
-  fs.readFileSync(new URL('../../package.json', import.meta.url), 'utf-8')
-);
+	fs.readFileSync(new URL('../../package.json', import.meta.url), 'utf8'),
+) as Package;
 updateNotifier({pkg}).notify();
 
 const cli = meow(
-  `
+	`
   Usage
     $ promotime GITHUB_USERNAME [--arguments]
 
@@ -34,64 +33,74 @@ const cli = meow(
   Examples
     $ promotime JustinBeckwith --start 2011-01-01 --end 2019-03-01
 `,
-  {
-    importMeta: import.meta,
-    flags: {
-      start: {type: 'string'},
-      end: {type: 'string'},
-    },
-  }
+	{
+		importMeta: import.meta,
+		flags: {
+			start: {type: 'string'},
+			end: {type: 'string'},
+		},
+	},
 );
 
-interface Results {
-  pullRequestsCreated?: number;
-  codeReviewsCompleted?: number;
-  issuesClosed?: number;
-  pullRequestCommentsMade?: number;
+type Results = {
+	pullRequestsCreated?: number;
+	codeReviewsCompleted?: number;
+	issuesClosed?: number;
+	pullRequestCommentsMade?: number;
+};
+
+type Payload = {
+	action: string;
+};
+
+type QueryResult = {
+	type: string;
+	payload: string;
+};
+
+if (cli.input.length !== 1) {
+	cli.showHelp();
 }
 
-async function main() {
-  if (cli.input.length !== 1) {
-    cli.showHelp();
-    return;
-  }
-  const user = cli.input[0];
-  const bq = new BigQuery();
-  let query = `SELECT * FROM \`githubarchive.month.*\` WHERE actor.login='${user}'`;
-  if (cli.flags.start) {
-    query += ` AND created_at > '${cli.flags.start}'`;
-  }
-  if (cli.flags.end) {
-    query += ` AND created_at < '${cli.flags.end}'`;
-  }
-  console.log(query);
-  const [data] = await bq.query(query);
-  const results = {} as Results;
-
-  // Get # of pull requests opened
-  results.pullRequestsCreated = data.filter(x => {
-    if (x.type === 'PullRequestEvent') {
-      const payload = JSON.parse(x.payload);
-      return payload.action === 'opened';
-    }
-    return false;
-  }).length;
-
-  // Get # of pull request comments
-  results.pullRequestCommentsMade = data.filter(x => {
-    return x.type === 'PullRequestReviewCommentEvent';
-  }).length;
-
-  // Get # of issues closed
-  results.issuesClosed = data.filter(x => {
-    if (x.type === 'IssuesEvent') {
-      const payload = JSON.parse(x.payload);
-      return payload.action === 'closed';
-    }
-    return false;
-  }).length;
-
-  console.log(results);
+const user = cli.input[0];
+const bq = new BigQuery();
+let query = `SELECT * FROM \`githubarchive.month.*\` WHERE actor.login='${user}'`;
+if (cli.flags.start) {
+	query += ` AND created_at > '${cli.flags.start}'`;
 }
 
-main();
+if (cli.flags.end) {
+	query += ` AND created_at < '${cli.flags.end}'`;
+}
+
+console.log(query);
+const [rawData] = await bq.query(query);
+const data = rawData as QueryResult[];
+const results: Results = {};
+
+// Get # of pull requests opened
+results.pullRequestsCreated = data.filter((x) => {
+	if (x.type === 'PullRequestEvent') {
+		const payload = JSON.parse(x.payload) as Payload;
+		return payload.action === 'opened';
+	}
+
+	return false;
+}).length;
+
+// Get # of pull request comments
+results.pullRequestCommentsMade = data.filter((x) => {
+	return x.type === 'PullRequestReviewCommentEvent';
+}).length;
+
+// Get # of issues closed
+results.issuesClosed = data.filter((x) => {
+	if (x.type === 'IssuesEvent') {
+		const payload = JSON.parse(x.payload) as Payload;
+		return payload.action === 'closed';
+	}
+
+	return false;
+}).length;
+
+console.log(results);
